03f462ffd1137e5062fa2d4ca330201336963825
dong-ju <spdlqj011@gmail.com>
2023-07-02 17:10:34 +0900

    Notion 정리용-Recap(6)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    // recap 실습 시 style 미적용
+    // <div className="">
     <div className="w-full max-w-xl mx-auto">
       <Component {...pageProps} />
     </div>
--- pages/test/recap/responsive.tsx
+++ pages/test/recap/responsive.tsx
@@ -6,29 +6,45 @@ const Home: NextPage = () => {
         // Mobile 화면(기본)에서는 행(row) 단위(grid)로 하위 3개 item을 표현하였으나,
         // 화면 크기가 커질때 열(column) 단위(grid-col)로 표현 변경
         <div className="bg-slate-500 px-10 py-20 grid gap-10 md:grid-cols-2 lg:grid-cols-3 min-h-screen lg:place-items-center">
-            <div className="bg-white rounded-3xl shadow-2xl p-6">
-                <span className="font-semibold text-3xl">Select Item</span>
+            {/* dark : DarkMode 시 적용될 스타일 지정가능 */}
+            <div className="bg-white dark:bg-gray-600 flex flex-col justify-between rounded-3xl shadow-2xl p-6">
+                <span className="font-semibold text-3xl dark:text-gray-100">
+                    Select Item
+                </span>
                 <div className="flex justify-between">
-                    <span className="text-gray-500 my-2">Grey Chair</span>
-                    <span className="font-semibold">$19</span>
+                    <span className="text-gray-500 dark:text-white my-2">
+                        Grey Chair
+                    </span>
+                    <span className="font-semibold dark:text-gray-100">
+                        $19
+                    </span>
                 </div>
                 <div className="flex justify-between">
-                    <span className="text-gray-500">Grey Chair</span>
-                    <span className="font-semibold">$19</span>
+                    <span className="text-gray-500 dark:text-white">
+                        Grey Chair
+                    </span>
+                    <span className="font-semibold dark:text-gray-100">
+                        $19
+                    </span>
                 </div>
                 <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-                    <span>Total</span>
-                    <span className="font-semibold">$38</span>
+                    <span className="dark:text-white">Total</span>
+                    <span className="font-semibold dark:text-gray-100">
+                        $38
+                    </span>
                 </div>
                 <div
                     className="mt-5 bg-blue-500 text-white p-3 w-1/2 mx-auto text-center rounded-xl hover:bg-teal-700 hover:text-black
-                            active:bg-yellow-500 focus:text-red-500"
+                    // 기존의 hover modifier를 dark:hover 라는 중첩 modifier로 calcelling 시킴
+                            dark:hover:bg-green-200 dark:hover:text-black active:bg-yellow-500 focus:text-red-500"
                 >
                     Checkout
                 </div>
             </div>
             <div className="bg-white overflow-hidden rounded-3xl shadow-2xl group">
-                <div className="bg-blue-500 p-6 pb-14">
+                {/* portrait : 기기가 세로모드일때 적용될 요소(기본)
+                landscape : 기기가 가로모드일때 적용될 요소 */}
+                <div className="portrait:bg-indigo-500 landscape:bg-teal-800 p-6 pb-14">
                     <span className="text-white text-2xl">Profile</span>
                 </div>
                 <div className="rounded-3xl bg-white relative -top-5 p-6">
